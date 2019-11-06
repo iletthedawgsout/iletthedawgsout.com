b38275c822e98e87cf851fe2ededc348598a1614
Luis Naranjo <jonaranj@microsoft.com>
2019-11-05 22:57:21 -0800

    add commander

--- backend/index.ts
+++ backend/index.ts
@@ -1,13 +1,30 @@
 import express from "express";
 import path from "path";
+import program from "commander";
 
 const app = express();
 
-const buildDir = path.join("..", "frontend", "build");
-const indexPath = path.join(buildDir, "index.html");
+const startServer = (buildDir: string, indexPath: string, port: number) => {
+    app.use(express.static(buildDir));
+    app.get('/', function (req, res) {
+        res.sendFile(indexPath);
+    });
+    app.listen(port);
+};
 
-app.use(express.static(buildDir));
-app.get('/', function(req, res) {
-  res.sendFile(indexPath);
-});
-app.listen(9000);
+program
+    .command("start")
+    .description("Start the server")
+    .option(
+      "-p, --production",
+      "Use production mode"
+    )
+    .action(options => {
+        const buildDir = !!options.production ? "/var/www" : path.join("..", "frontend", "build");
+        const indexPath = path.join(buildDir, "index.html");
+        const port = 9000;
+
+        console.log(`Starting server at port ${port}. Serving files from ${buildDir}`);
+
+        startServer(buildDir, indexPath, port);
+    });
