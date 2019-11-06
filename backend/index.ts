789435e573820fc2d402c881d21011be505c7c05
Luis Naranjo <jonaranj@microsoft.com>
2019-11-05 22:16:45 -0800

    fix paths

--- backend/index.ts
+++ backend/index.ts
@@ -2,8 +2,15 @@ import express from "express";
 import path from "path";
 
 const app = express();
-app.use(express.static(path.join(__dirname, 'build')));
+
+const buildDir = path.join("..", "frontend", "build");
+const indexPath = path.join(buildDir, "index.html");
+
+console.log(`buildDir: ${buildDir}`);
+console.log(`indexPath: ${indexPath}`);
+
+app.use(express.static(buildDir));
 app.get('/', function(req, res) {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+  res.sendFile(indexPath);
 });
 app.listen(9000);
