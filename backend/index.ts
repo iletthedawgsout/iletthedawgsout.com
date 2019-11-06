cb1397e0c61798765d0b2a1586b7604351a08d45
Luis Naranjo <jonaranj@microsoft.com>
2019-11-05 22:03:52 -0800

    add express

--- backend/index.ts
+++ backend/index.ts
@@ -1,3 +1,9 @@
-const hello = () => 'world';
+import express from "express";
+import path from "path";
 
-console.log(`hello: ${hello()}`);
+const app = express();
+app.use(express.static(path.join(__dirname, 'build')));
+app.get('/', function(req, res) {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+app.listen(9000);
