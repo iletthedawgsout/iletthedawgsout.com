5cf388078f8d9af5f4f1dd87a69a305f100aee28
Luis Naranjo <jonaranj@microsoft.com>
2019-12-07 11:05:28 -0800

    integrate redux

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,25 +1,27 @@
 import React from 'react';
-import logo from './logo.svg';
+import { store } from "./redux";
+import { Provider } from 'react-redux'
 import './App.css';
 
 const App: React.FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          GO DAWGS
-        </a>
-      </header>
-    </div>
+    <Provider store={store}>
+      <div className="App">
+        <header className="App-header">
+          <p>
+            Edit <code>src/App.tsx</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            GO DAWGS
+          </a>
+        </header>
+      </div>
+    </Provider>
   );
 }
 
