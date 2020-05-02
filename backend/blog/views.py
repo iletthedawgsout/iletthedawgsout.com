2b30f4baff71eae39aa2fcb3237ad678c5701b33
Luis Naranjo <jonaranj@microsoft.com>
2020-05-02 14:45:03 -0700

    check in part 3 tutorial

--- backend/blog/views.py
+++ backend/blog/views.py
@@ -1,19 +1,18 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, Http404
 from django.template import loader
+from django.shortcuts import render, get_object_or_404
 
 from .models import Question
 
 
 def index(request):
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    template = loader.get_template('blog/index.html')
-    context = {
-        'latest_question_list': latest_question_list,
-    }
-    return HttpResponse(template.render(context, request))
+    context = {'latest_question_list': latest_question_list}
+    return render(request, 'blog/index.html', context)
 
 def detail(request, question_id):
-    return HttpResponse("You're looking at question %s." % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'blog/detail.html', {'question': question})
 
 def results(request, question_id):
     response = "You're looking at the results of question %s."
