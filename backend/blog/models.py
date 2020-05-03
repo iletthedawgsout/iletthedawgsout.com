ecd4ef7ce94b3e93c60ee590714bab97b509e715
Luis Naranjo <jonaranj@microsoft.com>
2020-05-03 13:08:38 -0700

    add DRF tutorial integration

--- backend/blog/models.py
+++ backend/blog/models.py
@@ -1,6 +1,19 @@
+from django.contrib.auth.models import User, Group
+from rest_framework import serializers
 from django.db import models
 from django.utils import timezone
 import datetime
+
+class UserSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = User
+        fields = ['url', 'username', 'email', 'groups']
+
+
+class GroupSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Group
+        fields = ['url', 'name']
 
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
--- backend/iletthedawgsout/urls.py
+++ backend/iletthedawgsout/urls.py
@@ -1,7 +1,17 @@
 from django.contrib import admin
+from rest_framework import routers
 from django.urls import include, path
+from blog import views
 
+router = routers.DefaultRouter()
+router.register(r'users', views.UserViewSet)
+router.register(r'groups', views.GroupViewSet)
+
+# Wire up our API using automatic URL routing.
+# Additionally, we include login URLs for the browsable API.
 urlpatterns = [
+    path('', include(router.urls)),
+    path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
     path('blog/', include('blog.urls')),
     path('admin/', admin.site.urls),
 ]
