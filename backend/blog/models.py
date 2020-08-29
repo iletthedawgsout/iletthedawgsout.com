49540c6992a82b2f6a8ad74c189d45951e691e5a
Luis Naranjo <jonaranj@microsoft.com>
2020-08-29 15:34:19 -0700

    Iterate on blog post model

--- backend/blog/models.py
+++ backend/blog/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+import datetime
+
+from django.db import models
+from django.utils import timezone
+
+class Post(models.Model):
+    title = models.CharField(max_length=255)
+    publish_date = models.DateTimeField('date published', default=timezone.now)
+    visible = models.BooleanField(default=False)
+    # image = models.ImageField()
+    content = models.TextField(blank=True)
+    last_edited = models.DateTimeField(auto_now=True)
+    upvotes = models.IntegerField(blank=True, default=0)
+
+    def __str__(self):
+        return self.title
+
+
