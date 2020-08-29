2bee9a42f0cdb726e23429fabf01a39cde37b3a0
Luis Naranjo <jonaranj@microsoft.com>
2020-08-29 15:14:57 -0700

    replace polls with blog app

--- backend/azuresite/settings.py
+++ backend/azuresite/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'polls.apps.PollsConfig',
+    'blog.apps.BlogConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
