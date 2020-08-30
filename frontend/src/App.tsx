8b3c599ab750d410d6cd4158460dd988f2bfef93
Luis Naranjo <jonaranj@microsoft.com>
2020-08-29 21:43:05 -0700

    add react router

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,55 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 
 export default function App() {
   return (
-    <div>
-    </div>
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Blog</Link>
+            </li>
+            <li>
+              <Link to="/blm">BLM</Link>
+            </li>
+            <li>
+              <Link to="/about">About me</Link>
+            </li>
+          </ul>
+        </nav>
+
+        {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+       <Switch>
+          <Route path="/about">
+            <About />
+          </Route>
+          <Route path="/blm">
+            <BlackLivesMatter />
+          </Route>
+          <Route path="/">
+            <Blog />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
+}
+
+function Blog() {
+  return <h2>Blogs</h2>;
+}
+
+function BlackLivesMatter() {
+  return <h2>Black lives matter</h2>;
+}
+
+function About() {
+  return <h2>About me</h2>;
 }
