d34b174892a00fceb2a2636157f902897d9ef159
Luis Naranjo <jonaranj@microsoft.com>
2020-09-02 20:58:15 -0700

    Revert "rename api endpoint"
    This reverts commit 68ce0a4a7936bcaa3a5798e4c76b079d5f737d6e.

--- backend/blog/urls.py
+++ backend/blog/urls.py
@@ -3,7 +3,7 @@ from rest_framework import routers
 from blog import views
 
 router = routers.DefaultRouter()
-router.register(r'/posts', views.PostViewSet)
+router.register(r'posts', views.PostViewSet)
 
 # Wire up our API using automatic URL routing.
 # Additionally, we include login URLs for the browsable API.
