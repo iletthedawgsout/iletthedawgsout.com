2ea245b66594926e5c80a4b76d1420f396d8d7af
Luis Naranjo <jonaranj@microsoft.com>
2020-09-19 13:06:20 -0700

    Use a custom link

--- frontend/src/navigation/NavBar.tsx
+++ frontend/src/navigation/NavBar.tsx
@@ -1,18 +1,39 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 
-export const NavBar = (): JSX.Element => (
-    <nav style={{ flex: 1 }}>
-        <ul>
-            <li>
-                <Link to="/">Blog</Link>
-            </li>
-            <li>
-                <Link to="/blm">BLM</Link>
-            </li>
-            <li>
-                <Link to="/about">About me</Link>
-            </li>
-        </ul>
-    </nav>
-);
+type Props = Readonly<{
+    label: string;
+    to: string;
+}>;
+
+const CustomLink = ({ label, to }: Props) => {
+    let { pathname } = useLocation();
+    const match = pathname === to;
+
+    return (
+        <div className={match ? "active" : ""}>
+            {match && "> "}
+            <Link to={to}>{label}</Link>
+        </div>
+    );
+};
+
+
+export const NavBar = (): JSX.Element => {
+
+    return (
+        <nav style={{ flex: 1, borderStyle: "dotted" }}>
+            <ul style={{ listStyleType: "none" }}>
+                <li>
+                    <CustomLink to="/" label="Blog" />
+                </li>
+                <li>
+                    <CustomLink to="/blm" label="BLM" />
+                </li>
+                <li>
+                    <CustomLink to="/about" label="About" />
+                </li>
+            </ul>
+        </nav>
+    );
+};
