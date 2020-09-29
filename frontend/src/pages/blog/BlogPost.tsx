6e1cc9c0ba43d1589c6a3647739e30fbe58c092b
Luis Naranjo <jonaranj@microsoft.com>
2020-09-28 22:10:25 -0700

    styling

--- frontend/src/pages/blog/BlogPost.tsx
+++ frontend/src/pages/blog/BlogPost.tsx
@@ -1,10 +1,33 @@
 import React from 'react';
 import { Post } from '../../services/blog-posts';
+import { StyleSheet } from '../../models';
+
+const styles: StyleSheet = {
+    container: {
+        flex: 1,
+        display: 'flex',
+        flexDirection: "column",
+        alignItems: 'center',
+        justifyContent: 'center',
+        
+        borderStyle: "solid",
+    },
+    title: {
+
+    },
+    publish_date: {
+        
+    }
+};
 
 type Props = Readonly<{
     post: Post;
 }>;
 
 export const BlogPost = ({ post }: Props): JSX.Element => {
-    return <p>{post.title}</p>;
+    return <div style={styles.container}>
+        <p style={styles.title}>{post.title}</p>
+        <p style={styles.publish_date}>{post.publish_date}</p>
+        <p>{ post.source }</p>
+    </div>;
 };
