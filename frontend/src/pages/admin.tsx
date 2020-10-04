ec20e687cf56f275330365719f56c3851241e284
Luis Naranjo <jonaranj@microsoft.com>
2020-10-04 13:16:11 -0700

    add form for new post

--- frontend/src/pages/admin.tsx
+++ frontend/src/pages/admin.tsx
@@ -45,12 +45,61 @@ const ViewPosts = () => {
     return <ul>{items}</ul>;
 };
 
+const PostMaker = () => {
+    return (
+        <form action="/my-handling-form-page" method="post">
+            <ul>
+                <li>
+                    <label htmlFor="title">Title:</label>
+                    <input type="text" id="title" name="title" />
+                </li>
+
+                <li>
+                    <label htmlFor="publish_date">Start date:</label>
+                    <input
+                        type="date"
+                        id="publish_date"
+                        name="publish_date"
+                        value="2018-07-22"
+                        min="2018-01-01"
+                        max="2018-12-31"
+                        onChange={console.log}
+                    />
+                </li>
+
+                <li>
+                    <label htmlFor="visible">Visible</label>
+                    <input type="checkbox" id="visible" name="visible" onChange={console.log} />
+                </li>
+
+                <li>
+                    <label htmlFor="imgAltText">Image alt text: </label>
+                    <input type="text" id="imgAltText" name="imgAltText" />
+                </li>
+
+                <li>
+                    <label htmlFor="relativeImagePath">relativeImagePath: </label>
+                    <input type="text" id="relativeImagePath" name="relativeImagePath" />
+                </li>
+
+                <li>
+                    <label htmlFor="relativeMarkdownPath">relativeMarkdownPath: </label>
+                    <input type="text" id="relativeMarkdownPath" name="relativeMarkdownPath" />
+                </li>
+            </ul>
+
+            <button type="submit">Publish new blog post</button>
+        </form>
+    );
+};
+
 export const Admin = (): JSX.Element => {
     return (
         <RootContainer style={styles.container}>
             <h1> Admin</h1>
             <Link to={'/'}>{'Back to home'}</Link>
             <ViewPosts />
+            <PostMaker />
         </RootContainer>
     );
 };
